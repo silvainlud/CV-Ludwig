7d1d0abf417ef1d49128105ac6c75c478f4e6a34
Ludwig <ludwig.silvain@orange.fr>
2020-07-24 23:40:59 +0200

    ~ Update Symfony vers 5.1.3 + Séparation Header => Index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function Index()
+    public function Index(): Response
     {
-        return $this->render('index/base.html.twig');
+        return $this->render('index/index.html.twig');
     }
 }
