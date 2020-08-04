3520b9ea3dd0bbd46671bd154b0829ae1360e020
Ludwig <ludwig.silvain@orange.fr>
2020-08-04 23:33:40 +0200

    + Administration #4

--- src/Controller/Dashboard/CV/AdminSkillsController.php
+++ src/Controller/Dashboard/CV/AdminSkillsController.php
@@ -4,13 +4,18 @@ namespace App\Controller\Dashboard\CV;
 
 use App\Entity\Main\CV\Competence;
 use App\Entity\Main\CV\CompetenceCategorie;
+use App\Entity\Main\CV\Technologie;
 use App\Form\CV\CompetenceCategorieType;
 use App\Form\CV\CompetenceType;
+use App\Form\CV\TechnologieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdminSkillsController extends AbstractController
 {
@@ -19,16 +24,19 @@ class AdminSkillsController extends AbstractController
      */
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    private TranslatorInterface $translator;
+
+    public function __construct(EntityManagerInterface $em, TranslatorInterface $translator)
     {
         $this->em = $em;
+        $this->translator = $translator;
     }
 
     /**
      * @Route("/dashboard/cv/skills", name="db_cv_skills")
      * @Route("/dashboard/cv/skills/categories/{cat}", name="db_cv_skills_categories_view")
      */
-    public function View(Request $request, CompetenceCategorie $cat = null): Response
+    public function ViewCategories(Request $request, CompetenceCategorie $cat = null): Response
     {
         $newCat = new CompetenceCategorie();
         $formCat = $this->createForm(CompetenceCategorieType::class, $newCat);
@@ -102,6 +110,64 @@ class AdminSkillsController extends AbstractController
         }
 
         return $this->render('dashboard/cv/skills/edit_skill.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard/cv/skills/technlogies", name="db_cv_skills_technologies")
+     */
+    public function ViewTechnologies(Request $request)
+    {
+        $_t = new Technologie();
+        $form = $this->createForm(TechnologieType::class, $_t, ['cancel_btn' => false]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $this->em->persist($_t);
+
+                $this->em->flush();
+
+                return $this->redirectToRoute('db_cv_skills_technologies');
+            }
+            if (null == $form->get('upload')->getData() && null == $_t->getImage()) {
+                $form->get('upload')->addError(new FormError($this->translator->trans((new NotNull())->message, [], 'validators')));
+            }
+        }
+        $_ts = $this->em->getRepository(Technologie::class)->findAll();
+
+        return $this->render('dashboard/cv/skills/viewTechnologies.html.twig', [
+            'technologies' => $_ts,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard/cv/skills/technlogies/{tech}/edit", name="db_cv_skills_technologies_edit")
+     */
+    public function EditTechnology(Request $request, Technologie $tech): Response
+    {
+        $form = $this->createForm(TechnologieType::class, $tech, ['cancel_btn' => true]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $image = $form->get('upload')->getData();
+
+                if (null != $image) {
+                    $tech->setUpload($image);
+                }
+
+                $this->em->flush();
+
+                return $this->redirectToRoute('db_cv_skills_technologies');
+            }
+            if (null == $form->get('upload')->getData() && null == $_t->getImage()) {
+                $form->get('upload')->addError(new FormError($this->translator->trans((new NotNull())->message, [], 'validators')));
+            }
+        }
+
+        return $this->render('dashboard/cv/skills/edit_technlology.html.twig', [
             'form' => $form->createView(),
         ]);
     }
